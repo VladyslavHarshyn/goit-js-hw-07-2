e78bb9f8f458da3982d6014486cc51b79f163e59
Vladyslav Harshyn <garshyn_98@ukr.net>
2022-07-08 14:15:12 +0300

    I hope it's done properly

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function addElement() {
+  return galleryItems
+    .map(
+      (items) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${items.original}">
+    <img
+        class="gallery__image"
+        src="${items.preview}"
+        data-source="${items.original}"
+        alt="${items.description}"
+    />
+    </a>
+</div>`
+    )
+    .join("");
+}
+gallery.insertAdjacentHTML("afterbegin", addElement(galleryItems));
+
+gallery.addEventListener("click", galleryClick);
+
+let instantShow;
+
+function galleryClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  instantShow = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `);
+
+  instantShow.show();
+}
+
+// Закрытие модалки по нажатию на Esc
+
+gallery.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") instantShow.close();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function addElement() {
+  return galleryItems
+    .map(
+      (items) =>
+        `<a class="gallery__item" href="${items.original}">
+            <img
+              class="gallery__image"
+              src="${items.preview}"
+              alt="${items.description}"
+            />
+          </a>`
+    )
+    .join("");
+}
+gallery.insertAdjacentHTML("afterbegin", addElement(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
